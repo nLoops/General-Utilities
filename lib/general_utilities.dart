15de0efbc4343f2f8217b0ef87c56097ccc86a59
Ahmed Ibrahim <a.elgammal112@gmail.com>
2020-06-19 23:43:31 +0200

    feat: export widgets & add package home page.

--- lib/general_utilities.dart
+++ lib/general_utilities.dart
@@ -1,7 +1,6 @@
-library general_utilities;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/data_loading_widget.dart';
+export 'src/footer_layout.dart';
+export 'src/no_data_widget.dart';
+export 'src/restart_widget.dart';
+export 'src/route_animation.dart';
+export 'src/widget_animator.dart';
